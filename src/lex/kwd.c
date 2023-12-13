171cc9a92cff37ddb5622c85dda8decc05d4199d
Piggy <piggynl@outlook.com>
2023-12-13 14:24:06 +0800

    lex/kwd: accept all keywords

--- src/lex/kwd.c
+++ src/lex/kwd.c
@@ -5,7 +5,7 @@
 
 Kwd matchKwd(const char *begin, const char *end) {
   for (Kwd k = KWD_INVAL; k < KWD_LEN; k++) {
-    if (end - begin == kwdInfo[k].strlen) {
+    if (begin[0] == kwdInfo[k].str[0] && end - begin == kwdInfo[k].strlen) {
       if (memcmp(begin, kwdInfo[k].str, end - begin) == 0)
         return k;
     }
@@ -20,12 +20,152 @@ KwdInfoItem kwdInfo[KWD_LEN] = {
     .str = "<@invalid kwd@>",
     .strlen = 0,
   },
+  [KWD_AUTO] = {
+    .str = "auto",
+    .strlen = 4,
+  },
+  [KWD_BREAK] = {
+    .str = "break",
+    .strlen = 5,
+  },
+  [KWD_CASE] = {
+    .str = "case",
+    .strlen = 4,
+  },
+  [KWD_CHAR] = {
+    .str = "char",
+    .strlen = 4,
+  },
+  [KWD_CONST] = {
+    .str = "const",
+    .strlen = 5,
+  },
+  [KWD_CONTINUE] = {
+    .str = "continue",
+    .strlen = 8,
+  },
+  [KWD_DEFAULT] = {
+    .str = "default",
+    .strlen = 7,
+  },
+  [KWD_DO] = {
+    .str = "do",
+    .strlen = 2,
+  },
+  [KWD_DOUBLE] = {
+    .str = "double",
+    .strlen = 6,
+  },
+  [KWD_ELSE] = {
+    .str = "else",
+    .strlen = 4,
+  },
+  [KWD_ENUM] = {
+    .str = "enum",
+    .strlen = 4,
+  },
+  [KWD_EXTERN] = {
+    .str = "extern",
+    .strlen = 6,
+  },
+  [KWD_FLOAT] = {
+    .str = "float",
+    .strlen = 5,
+  },
+  [KWD_FOR] = {
+    .str = "for",
+    .strlen = 3,
+  },
+  [KWD_GOTO] = {
+    .str = "goto",
+    .strlen = 4,
+  },
+  [KWD_IF] = {
+    .str = "if",
+    .strlen = 2,
+  },
+  [KWD_INLINE] = {
+    .str = "inline",
+    .strlen = 6,
+  },
   [KWD_INT] = {
     .str = "int",
     .strlen = 3,
   },
+  [KWD_LONG] = {
+    .str = "long",
+    .strlen = 4,
+  },
+  [KWD_REGISTER] = {
+    .str = "register",
+    .strlen = 8,
+  },
+  [KWD_RESTRICT] = {
+    .str = "restrict",
+    .strlen = 8,
+  },
   [KWD_RETURN] = {
     .str = "return",
     .strlen = 6,
+  },
+  [KWD_SHORT] = {
+    .str = "short",
+    .strlen = 5,
+  },
+  [KWD_SIGNED] = {
+    .str = "signed",
+    .strlen = 6,
+  },
+  [KWD_SIZEOF] = {
+    .str = "sizeof",
+    .strlen = 6,
+  },
+  [KWD_STATIC] = {
+    .str = "static",
+    .strlen = 6,
+  },
+  [KWD_STRUCT] = {
+    .str = "struct",
+    .strlen = 6,
+  },
+  [KWD_SWITCH] = {
+    .str = "switch",
+    .strlen = 6,
+  },
+  [KWD_TYPEDEF] = {
+    .str = "typedef",
+    .strlen = 7,
+  },
+  [KWD_UNION] = {
+    .str = "union",
+    .strlen = 5,
+  },
+  [KWD_UNSIGNED] = {
+    .str = "unsigned",
+    .strlen = 8,
+  },
+  [KWD_VOID] = {
+    .str = "void",
+    .strlen = 4,
+  },
+  [KWD_VOLATILE] = {
+    .str = "volatile",
+    .strlen = 8,
+  },
+  [KWD_WHILE] = {
+    .str = "while",
+    .strlen = 5,
+  },
+  [KWD_BOOL] = {
+    .str = "_Bool",
+    .strlen = 5,
+  },
+  [KWD_COMPLEX] = {
+    .str = "_Complex",
+    .strlen = 8,
+  },
+  [KWD_IMAGINARY] = {
+    .str = "_Imaginary",
+    .strlen = 10,
   },
 };
--- src/lex/kwd.h
+++ src/lex/kwd.h
@@ -6,8 +6,59 @@
 enum Kwd {
   KWD_INVAL,
 
+  KWD_AUTO,
+
+  KWD_BREAK,
+
+  KWD_CASE,
+  KWD_CHAR,
+  KWD_CONST,
+  KWD_CONTINUE,
+
+  KWD_DEFAULT,
+  KWD_DO,
+  KWD_DOUBLE,
+
+  KWD_ELSE,
+  KWD_ENUM,
+  KWD_EXTERN,
+
+  KWD_FLOAT,
+  KWD_FOR,
+
+  KWD_GOTO,
+
+  KWD_IF,
+  KWD_INLINE,
   KWD_INT,
+
+  KWD_LONG,
+
+  KWD_REGISTER,
+  KWD_RESTRICT,
   KWD_RETURN,
+
+  KWD_SHORT,
+  KWD_SIGNED,
+  KWD_SIZEOF,
+  KWD_STATIC,
+  KWD_STRUCT,
+  KWD_SWITCH,
+
+  KWD_TYPEDEF,
+
+  KWD_UNION,
+
+  KWD_UNSIGNED,
+
+  KWD_VOID,
+  KWD_VOLATILE,
+
+  KWD_WHILE,
+
+  KWD_BOOL,      // _Bool
+  KWD_COMPLEX,   // _Complex
+  KWD_IMAGINARY, // _Imaginary
 
   KWD_LEN,
 };
