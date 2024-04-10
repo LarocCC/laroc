c10b94f32f05268be60d9ccea1e870897cdb768b
Piggy <piggynl@outlook.com>
2024-04-11 04:17:57 +0800

    lex/number: support octal and hexadecimal number

--- include/sema/number.h
+++ include/sema/number.h
@@ -5,13 +5,24 @@
 
 #include "sema/type.h"
 
+typedef enum NumberRadix {
+  // C99 (6.4.4.1) octal-constant
+  NUM_OCT = 8,
+  // C99 (6.4.4.1) decimal-constant
+  // C99 (6.4.4.2) decimal-floating-constant
+  NUM_DEC = 10,
+  // C99 (6.4.4.1) hexadecimal-constant
+  // C99 (6.4.4.2) hexadecimal-floating-constant
+  NUM_HEX = 16,
+} NumberRadix;
+
 /// A C integer constant or floating constant.
 ///
-/// TODO: Support hexadecimal and octal integer constant.
 /// TODO: Support integer suffix.
 /// TODO: Support floating constant.
 struct Number {
   uint64_t x;
+  NumberRadix radix;
   CType *ty;
 };
 
--- src/lex/number.c
+++ src/lex/number.c
@@ -1,5 +1,6 @@
+#include <assert.h>
 #include <ctype.h>
-#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 
 #include "sema/number.h"
@@ -9,10 +10,46 @@ int scanNumber(const char *begin, const char *end, Number *num) {
   const char *p = begin;
   uint64_t x = 0;
 
-  while (p < end && isdigit(*p)) {
-    // TODO: Handle overflowed number.
-    x = x * 10 + (*p - '0');
-    p++;
+  num->radix = NUM_DEC;
+  if (*p == '0') {
+    if (p + 1 < end && (*(p + 1) == 'x' || *(p + 1) == 'X')) {
+      num->radix = NUM_HEX;
+      p += 2;
+    } else {
+      num->radix = NUM_OCT;
+      p += 1;
+    }
+  }
+
+  // TODO: Handle overflowed number.
+  switch (num->radix) {
+  case NUM_OCT:
+    while (p < end && *p >= '0' && *p <= '9') {
+      x = x * 8 + (*p - '0');
+      p++;
+    }
+    break;
+
+  case NUM_DEC:
+    while (p < end && isdigit(*p)) {
+      x = x * 10 + (*p - '0');
+      p++;
+    }
+    break;
+
+  case NUM_HEX:
+    while (p < end && isxdigit(*p)) {
+      if (isdigit(*p)) {
+        x = x * 16 + (*p - '0');
+      } else {
+        x = x * 16 + 10 + (tolower(*p) - 'a');
+      }
+      p++;
+    }
+    break;
+
+  default:
+    assert(false && "Unknown radix");
   }
 
   num->x = x;
@@ -20,4 +57,19 @@ int scanNumber(const char *begin, const char *end, Number *num) {
   return p - begin;
 }
 
-void printNumber(const Number *num) { printf("%lu", num->x); }
+void printNumber(const Number *num) {
+  switch (num->radix) {
+  case NUM_OCT:
+    printf("(Oct) ");
+    break;
+  case NUM_DEC:
+    printf("(Dec) ");
+    break;
+  case NUM_HEX:
+    printf("(Hex) ");
+    break;
+  default:
+    assert(false && "Unknown radix");
+  }
+  printf("%lu", num->x);
+}
