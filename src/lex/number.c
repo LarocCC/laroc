612996eabdc1deb086111a6175d4e7ac19e12ed7
Piggy <piggynl@outlook.com>
2024-04-24 11:39:30 +0800

    lex/number: scan integers using strtoull()

--- src/lex/number.c
+++ src/lex/number.c
@@ -1,8 +1,10 @@
 #include <assert.h>
 #include <ctype.h>
+#include <errno.h>
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "typedef.h"
 #include "lex/lex.h"
@@ -14,49 +16,25 @@ static CType *inferIntegerType(Number *num);
 
 int scanNumber(LexCtx *ctx, const char *begin, const char *end, Number *num) {
   const char *p = begin;
-  uint64_t x = 0;
 
   num->radix = NUM_DEC;
   if (*p == '0') {
     if (p + 1 < end && (*(p + 1) == 'x' || *(p + 1) == 'X')) {
       num->radix = NUM_HEX;
-      p += 2;
     } else {
       num->radix = NUM_OCT;
-      p += 1;
     }
   }
 
-  // TODO: Handle overflowed number.
-  switch (num->radix) {
-  case NUM_OCT:
-    while (p < end && *p >= '0' && *p <= '9') {
-      x = x * 8 + (*p - '0');
-      p++;
-    }
-    break;
-
-  case NUM_DEC:
-    while (p < end && isdigit(*p)) {
-      x = x * 10 + (*p - '0');
-      p++;
-    }
-    break;
-
-  case NUM_HEX:
-    while (p < end && isxdigit(*p)) {
-      if (isdigit(*p)) {
-        x = x * 16 + (*p - '0');
-      } else {
-        x = x * 16 + 10 + (tolower(*p) - 'a');
-      }
-      p++;
-    }
-    break;
-
-  default:
-    assert(false && "Unknown radix");
+  char *num_end;
+  num->x = strtoull(p, &num_end, num->radix);
+  if (errno == ERANGE) {
+    updateContextTo(ctx, p);
+    SourceLoc *loc = newSourceLoc(ctx->lineno, ctx->col);
+    emitDiagnostic(loc, "The integer cannot be represented");
   }
+  assert(num_end != p);
+  p = num_end;
 
   while (isalpha(*p)) {
     switch (*p) {
@@ -105,8 +83,6 @@ int scanNumber(LexCtx *ctx, const char *begin, const char *end, Number *num) {
       emitDiagnostic(loc, "Invalid integer suffix '%c'", *p);
     }
   }
-
-  num->x = x;
 
   num->ty = inferIntegerType(num);
   if (!num->ty) {
